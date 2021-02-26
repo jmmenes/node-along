1950971426145126d2b5dafc1b9e2e12cc1889c7
John Meneses <jmmenes90@gmail.com>
2021-02-26 12:28:24 -0500

    Made changes.

--- mental-age.js
+++ mental-age.js
@@ -9,11 +9,11 @@ if (userAge === "--help") {
   console.log('Please enter your age after "node mental-age.js" and a space.');
 }
 
-// 3. divide it by 2 and add 3
-const mentalAge = userAge / 2 + 3;
+// 3. print out the user's age in a sentence
+// For example: "You are 23 years old."
 const ageNextYear = Number(userAge) + 1;
 console.log("Next year you'll be " + ageNextYear + " years old.");
 
-// 4. print out the user's age in a sentence
-// For example: "You are 23 years old."
+// 4. divide it by 2 and add 3
+const mentalAge = userAge / 2 + 3;
 console.log("Your mental age is " + mentalAge + " years old.");
